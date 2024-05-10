2315d70e1d111843615719442342a0d7fad100b2
AlexCarneiroo <121904833+AlexCarneiroo@users.noreply.github.com>
2024-05-10 20:24:34 -0300

    teste 2

--- src/componets/header/index.tsx
+++ src/componets/header/index.tsx
@@ -1,9 +1,6 @@
 import * as C from './styled';
 import logoImg from '../../imgs/WhatsApp Image 2023-06-03 at 07.54.14.jpeg';
 import verificado from '../../imgs/simbolo-verificado.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
 
 
 export const ContHeader = () =>{
@@ -19,8 +16,7 @@ export const ContHeader = () =>{
                 </C.namLoja>
                 <C.h4>Somos o maior vendedor de prata do Brasil!</C.h4>
                 <C.descricao>
-                    <C.inco><FontAwesomeIcon icon={faEnvelope} /></C.inco>
-                    <C.span href='picodaspratas000@gmail.com'>Email</C.span>
+                    <C.span href='mailto:picodaspratas000@gmail.com?subject=Assunto%20da%20Mensagem' target='_blank'>picodaspratas000@gmail.com</C.span>
                 </C.descricao>
             </C.Header>
         </C.Container>
